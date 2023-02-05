fa6e5b6168e9477b70e89840d72fd4422e8bcea5
Ender Çelik <64286011+EnderCelik0@users.noreply.github.com>
2023-02-05 19:08:39 +0300

    finish api call and display the api content in advice panel

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
 import './App.css'
 import AdvicePanel from './components/AdvicePanel'
 
 export default function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
-      <AdvicePanel/>
+      <AdvicePanel />
     </div>
   )
 }
--- src/components/AdvicePanel.jsx
+++ src/components/AdvicePanel.jsx
@@ -1,15 +1,28 @@
 import diceIcon from '../images/icon-dice.svg';
 import dividerMobile from '../images/divider-mobile.svg';
+import useFetch from './useFetch';
+
 export default function AdvicePanel() {
+  const {
+    data: advice,
+    loading,
+    error,
+    refetch,
+  } = useFetch('https://api.adviceslip.com/advice');
+
+  if (loading) return <q className='loading'>Loading...</q>;
+  if (error) console.log(error);
+
+  function getAdvice() {
+    refetch();
+  }
+
   return (
     <main className='advice-panel-container'>
-      <h6 className='advice-number'>ADVICE #117</h6>
-      <q className='quote'>
-        It is easy to sit up and take notice, what's difficult is getting up and
-        taking action.
-      </q>
-      <img src={dividerMobile} className="divider"></img>
-      <button className='btn-advice'>
+      <h6 className='advice-number'>ADVICE #{advice?.slip.id}</h6>
+      <q className='quote'>{advice?.slip.advice}</q>
+      <img src={dividerMobile} className='divider'></img>
+      <button onClick={refetch} className='btn-advice'>
         <img className='dice-icon' src={diceIcon}></img>
       </button>
     </main>
